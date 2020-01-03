a1a692b0f18805a6965d06cea36419e74eb47d96
Саша <C:\Users\\AppData\Roaming\The Bat!>
2020-01-03 12:20:34 +0200

    tasks Method references added

---
+++ Ascending and descending sorting/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.function.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isAscending = scanner.nextLine().equals("ascending");
+        int[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        BiFunction<Integer, Integer, Integer> comparator = null;
+        comparator = isAscending ?  Math::min : Math::max;
+        //comparator = isAscending? (first, second)->first.compareTo(second):(a, b) ->b.compareTo(a);
+        sort(array, comparator);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+
+    public static void sort(int[] array, BiFunction<Integer, Integer, Integer> comparator) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (comparator.apply(array[j], array[j + 1]) == array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
