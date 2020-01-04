8da0d2360f6637787ccf59cfc8e84c33e65c8896
Саша <C:\Users\\AppData\Roaming\The Bat!>
2020-01-04 12:40:51 +0200

    added and solved task An infinite numbers supplier

--- An infinite numbers supplier/src/Main.java
+++ An infinite numbers supplier/src/Main.java
@@ -1,38 +1,48 @@
 // Posted from EduTools plugin
-// Posted from EduTools plugin
+
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.*;
 import java.util.stream.*;
 
 
 class FunctionUtils {
-   /* public static void main(String[] args) {
+   /* static int number = new Scanner(System.in).nextInt();
+    public static void main(String[] args) {
         Supplier<Integer> sup = getInfiniteRange();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.print(sup.get() + " ");
         }
         System.out.println("");
         Supplier<Integer> sup1 = getInfiniteRange();
         Supplier<Integer> sup2 = getInfiniteRange();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.print(sup1.get() + " " + sup2.get() + " ");
         }
     }*/
+   
+        public static Supplier<Integer> getInfiniteRange() {
+            return IntStream.iterate(0, i -> i + 1).iterator()::next;
+        }
 
-    public static Supplier<Integer> getInfiniteRange() {
+    }
+
+   /* public static Supplier<Integer> getInfiniteRange() {
         List<Integer> list = Stream.iterate(0, i -> i + 1)
-                .limit(100000)
-                .collect(Collectors.toList());
+                .limit(212513)
+                .collect(Collectors.toCollection(ArrayList::new));
         //Supplier<Integer> suppplier =
+        final int[] count = {list.get(0)};
         return () -> {
-            int count = 0;
-            int result = list.get(count);
-            if (count < list.size()-1) {
-                list.remove(count++);
+            int result = 0;
+            if (count[0] < list.size()-1) {
+                result = list.get(count[0]++);
             }
+
+
             return result;
         };
-    }
+    }*/
 
 }
