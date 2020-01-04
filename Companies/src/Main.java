96af63a5da29bd5d1fbe57c66abc30cfd178f99f
Саша <C:\Users\\AppData\Roaming\The Bat!>
2020-01-04 12:00:52 +0200

    task store all strings to the list

--- Companies/src/Main.java
+++ Companies/src/Main.java
@@ -1,9 +1,14 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
-        String[]array = new Scanner(System.in).nextLine().split("\\s+");
-        System.out.println(Arrays.toString(array));
+       // String[] array = new Scanner(System.in).nextLine().split("\\s+");
+       // System.out.println(Arrays.toString(array));
+        List<String> list = Arrays.stream(new Scanner(System.in).nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+        System.out.println(list);
     }
 }
