b0affea939b4623e78a776e0ab3095720635dea9
Саша <C:\Users\\AppData\Roaming\The Bat!>
2020-01-04 11:57:46 +0200

    task store all strings to the array

---
+++ Companies/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        String[]array = new Scanner(System.in).nextLine().split("\\s+");
+        System.out.println(Arrays.toString(array));
+    }
+}
